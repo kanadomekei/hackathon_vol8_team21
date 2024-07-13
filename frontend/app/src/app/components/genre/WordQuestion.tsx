d04ddcdfaa77b21156236371b3c2ac60ab647d53
naokicode <kitajimanaoki.0917@icloud.com>
2024-07-13 16:47:16 +0900

    wordpage

--- frontend/app/src/app/components/genre/WordQuestion.tsx
+++ frontend/app/src/app/components/genre/WordQuestion.tsx
@@ -7,7 +7,7 @@ type WordQuestionProps = {
 const WordQuestion: React.FC<WordQuestionProps> = ({ trem  }) => {
   return (
     <div className="flex items-center justify-center h-1/2 border-b">
-      <h1 className="text-3xl">{word}</h1>
+      <h1 className="text-3xl">{trem}</h1>
     </div>
   );
 };
